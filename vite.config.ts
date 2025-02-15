e3cded649b20ae02b37235de60a9320a56e3574b
sudevifoods <sudevifoods@gmail.com>
2025-02-15 09:53:56 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/homepage/', // Replace 'sudevi' with your repository name
+  base: '/online/', // Replace 'sudevi' with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
